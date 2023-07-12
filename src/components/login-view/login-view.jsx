95d357ab7f775feb9f5bb75a4dc659ba11459afd
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-07-12 14:52:29 -0500

    added login and signup components successfully

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,50 +1,61 @@
 import React, { useState } from "react";
 
 export const LoginView = ({ onLoggedIn }) => {
-  const [Username, setUsername] = useState("");
-  const [Password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const data = {
-      Username: Username,
-      Password: Password
+      Username: username,
+      Password: password
     };
 
     fetch("https://tyflixdb-abb12f7ad46c.herokuapp.com/login", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(data)
-    }).then((response) => {
-      if (response.ok) {
-        onLoggedIn(Username);
-      } else {
-        alert("Login Failed");
-      }
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input 
+        <input
           type="text"
-          value={Username}
+          value={username}
           onChange={(e) => setUsername(e.target.value)}
-          required
         />
       </label>
       <label>
         Password:
         <input
-          type="password"
-          value={Password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         />
       </label>
-      <button type="submit">Submit</button>
+      <button type="submit">
+        Submit
+      </button>
     </form>
   );
 };
